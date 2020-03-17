33e09914fc697b5ab6416494168ca6a3d6259f64
vagrant <vagrant@localhost.localdomain>
2020-03-17 14:51:17 +0000

    [add] #3 環境設定途中まで - settings.py -> rest系frameworkの設定 - apiアプリ作成&urls, views作成(最低限) - vue.config.js -> localでのIP設定, ビルド先の設定 - eslint~~~ -> タブとインデントのエラー吐き出しを許容設定 - package.json -> 毎ビルド時にファイル生成されないように設定
    お腹痛くなっちゃったから一旦ここまで...

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,20 @@
+const BundleTracker = require('webpack-bundle-tracker')
+
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    transpileDependencies: [
+        'vuetify'
+    ],
+    // ローカルサーバーのIP設定
+    devServer: {
+      port: 8080,
+      host: '192.168.33.12',
+    },
+    // serverで展開する
+    outputDir: '../server',
+    // サーバーを起動したときのルートパス
+    publicPath: '/',
+    // outputDir起点で、index.htmlを格納する場所
+    indexPath: 'templates/index.html',
+    // outputDir起点で、staticファイルを格納する場所
+    assetsDir: 'static'
 }
