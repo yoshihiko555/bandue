9f6d546b47c97af5685b2d07582db80371e4fb95
vagrant <vagrant@localhost.localdomain>
2020-05-04 11:42:30 +0000

    [clean] デフォルトファイルの整理

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,25 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import SignUp from '@/components/register/SignUp.vue'
 import SignIn from '@/components/register/SignIn.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
   // 試しに定義してみたけど、あまり関係なさそう
   // そもそもルーティングはDjangoで行っているから
   {
