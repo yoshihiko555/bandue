06d14af88cbdb32777619ba7601db2f1ecc93846
krystasis <kRysTasis12@gmail.com>
2020-08-19 09:29:54 +0000

    [add] #43 Herokuコネクション問題一時解決
    Herokuデプロイ時にコネクションが閉じない問題一応解決。
    コネクションの有効時間設定したい時どうするかは、後々検討

--- server/server/settings.py
+++ server/server/settings.py
@@ -177,7 +177,7 @@ DATABASES = {
 }
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+db_from_env = dj_database_url.config(default=DATABASE_URL, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
